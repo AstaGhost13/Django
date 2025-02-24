cdf364db7b4bf6f4a5a2bc0daf58af22989a08ca
NavaCorp26 <luisnavarrete1a89@gmail.com>
2025-02-23 20:49:57 -0500

    Formularios mejorados

--- hierarchy/forms.py
+++ hierarchy/forms.py
@@ -4,22 +4,76 @@ from .models import Custodiam, Floor, Department ,Position
 class FloorForm(forms.ModelForm):
     class Meta:
         model = Floor
-        fields = ['description', 'status']
+        fields = ['status', 'description']
+        widgets = {
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese la descripción'}),
+            
+        }
 
 
 class DepartmentForm(forms.ModelForm):
     class Meta:
         model = Department
-        fields = ['description', 'status', 'parent' , 'id_floor']
+        fields = ['status', 'description', 'parent', 'id_floor']
+        widgets = {
+            'description': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese la descripción',
+            }),
+            'parent': forms.Select(attrs={
+                'class': 'form-control custom-select',
+            }),
+            'id_floor': forms.Select(attrs={
+                'class': 'form-control custom-select',
+            }),
+        }
 
-    
 class PositionForm(forms.ModelForm):
     class Meta:
         model = Position
-        fields = ['description', 'status', 'department']
+        fields = ['status', 'description', 'department']
+        widgets = {
+            'description': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese la descripción',
+            }),
+            'department': forms.Select(attrs={
+                'class': 'form-control custom-select',
+            }),
+        }
+
 
 
 class CustodiamForm(forms.ModelForm):
     class Meta:
         model = Custodiam
-        fields = ['status','first_name', 'last_name', 'phone_number', 'address', 'reference', 'email', 'position']
+        fields = ['status', 'first_name', 'last_name', 'phone_number', 'address', 'reference', 'email', 'position']
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el nombre',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el apellido',
+            }),
+            'phone_number': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el número de teléfono',
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese la dirección',
+            }),
+            'reference': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese la referencia',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el correo electrónico',
+            }),
+            'position': forms.Select(attrs={
+                'class': 'form-control custom-select',
+            })
+        }

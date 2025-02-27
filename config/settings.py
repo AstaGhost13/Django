95ea5f3c2d921a29b02f11312c5ae47b6c758b1f
AstaGhost13 <Sanchezandra2002@hotmail.com>
2025-02-26 20:52:50 -0500

    Cambio realizado 20:52

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k1q9kg5by*exc)if$(qx=8tg&c(_7!j9c4$g7e3k!e0a!itogy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-elu7.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
